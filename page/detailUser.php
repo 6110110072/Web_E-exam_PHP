064ad7251b867c4867a6f3d93eecff1af2069728
6110110072 <6110110072@psu.ac.th>
2021-04-17 12:45:34 +0700

    third

--- page/detailUser.php
+++ page/detailUser.php
@@ -64,6 +64,7 @@ if (isset($_GET['username'])) {
             </div>
             <br>
             Youtube link : <a href="<?php echo $youtubelink; ?>"><?php echo $youtubelink; ?></a>
+            <iframe width="560" height="315" src="<?php echo $youtubelink ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <br><br>
         </div>
 
